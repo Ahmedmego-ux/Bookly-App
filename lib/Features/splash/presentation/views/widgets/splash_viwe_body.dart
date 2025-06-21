8909f0ef6f8fe41c04c23b06f80faac6061b1651
ahmed <ahmedmagdy0102926@gmail.com>
2025-06-22 00:03:32 +0300

    creat homeview&navigate to homeview

--- lib/Features/splash/presentation/views/widgets/splash_viwe_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_viwe_body.dart
@@ -1,27 +1,24 @@
+import 'package:booklyapp/Features/home/presintation/view/home_view.dart';
 import 'package:booklyapp/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:booklyapp/constans.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
  class SplashViweBody extends StatefulWidget{
   const SplashViweBody({super.key});
-
   @override
   State<SplashViweBody> createState() => _SplashViweBodyState();
 }
-
-
 class _SplashViweBodyState extends State<SplashViweBody> with SingleTickerProviderStateMixin{
   late AnimationController animationController;
   late Animation <Offset>slidinganimation;
   @override
   void initState() {
-   
     super.initState();
-    animationController=AnimationController(vsync: this,duration: Duration(seconds: 1));
-    slidinganimation =Tween<Offset>(begin: Offset(0, 2),end: Offset.zero).animate(animationController);
-    animationController.forward();
-
+    initanimationcontroler();
+    NavigatortoHome();
   }
   @override
   void dispose() {
@@ -46,6 +43,16 @@ class _SplashViweBodyState extends State<SplashViweBody> with SingleTickerProvid
         ),
       ],
     );
+  }
+  void initanimationcontroler() {
+    animationController=AnimationController(vsync: this,duration: Duration(seconds: 1));
+    slidinganimation =Tween<Offset>(begin: Offset(0, 2),end: Offset.zero).animate(animationController);
+    animationController.forward();
+  }
+  void NavigatortoHome() {
+     Future.delayed(Duration(seconds: 2),(){
+    Get.to(()=>HomeView(),transition:Transition.fade,duration: ktranstionduration );
+    });
   }
 }
 
--- lib/constans.dart
+++ lib/constans.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kprimarycolor= Color(0xff100B20);
+const ktranstionduration =Duration(milliseconds: 250);
