2ce577f930ae5e5cf579cbec661e9e61fcb5f8ab
ahmed <ahmedmagdy0102926@gmail.com>
2025-07-02 18:51:01 +0300

    creat book details view

--- lib/Features/home/presintation/view/home_view.dart
+++ lib/Features/home/presintation/view/home_view.dart
@@ -1,4 +1,4 @@
-import 'package:booklyapp/Features/home/presintation/view/home_view_body.dart';
+import 'package:booklyapp/Features/home/presintation/view/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget{
