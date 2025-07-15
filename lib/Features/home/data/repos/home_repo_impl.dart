54a4b7ddc3926000783e5050adc83af7de882ffc
ahmed <ahmedmagdy0102926@gmail.com>
2025-07-15 22:44:53 +0300

    implement fetchbestseller books

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
+import 'package:booklyapp/core/erorrs/failure.dart';
+import 'package:booklyapp/core/utils/api_service.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks()async {
+   try {
+  var data=await apiService.get(url: 'volumes?Filtering=free-ebooks&q=subject:programming');
+  List<BookModel> books=[];
+  for (var item in data['items']) {
+    books.add(BookModel.fromJson(item));
+  }
+  return right(books);
+}  catch (e) {
+  return left(serverfailure());
+}
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
